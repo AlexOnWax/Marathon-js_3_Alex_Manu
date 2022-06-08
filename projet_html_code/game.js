3daa3eec6a3df09734091a903dff1a3e4e84cf9c
AlexOnWax <alexandre.barbet@hotmail.com>
2022-06-08 21:00:22 +0200

    Game.js est fini

--- projet_html_code/game.js
+++ projet_html_code/game.js
@@ -4,92 +4,94 @@ const rock = document.querySelector(".rock");
 const lesMains = document.querySelectorAll(".option-container");
 const step1 = document.querySelector(".step-1");
 const step2 = document.querySelector(".step-2");
-const playerPick = document.querySelector(".player-pick");
 const playAgainButton = document.querySelector(".play-again-btn");
-const computerPick = document.querySelector(".computer-pick");
 const replaces = document.querySelectorAll(".replace");
-const picks = document.querySelectorAll(".pick");
 scissors2 = scissors.cloneNode(true);
 paper2 = paper.cloneNode(true);
 rock2 = rock.cloneNode(true);
-const un = document.querySelector('#un')
 const tabReplace = [replaces[0], replaces[1]];
 const playerPickReplace = document.querySelector(".player-pick > .replace");
 const compPickReplace = document.querySelector(".computer-pick > .replace");
-const tabPfp=[scissors, rock, paper]
+const tabPfp = [scissors, rock, paper];
 const tab = [scissors2, rock2, paper2];
-const imgScissors = document.querySelector('.scissors > option > img ')
-   const WinnerLooserText = document.querySelector('.annouce-winner-text') 
+const scoreValue = document.querySelector(".score-value");
+const imgScissors = document.querySelector(".scissors > option > img ");
+const WinnerLooserText = document.querySelector(".annouce-winner-text");
+let resultat = 0;
 
 
-    function randomComputer(x) {
+function randomComputer(x) {
   return Math.floor(Math.random() * x);
 }
 
-
-
-
 lesMains.forEach((main) => {
-    const scoreValue = document.querySelector(".score-value");
-    let resultat = 0;
-main.addEventListener('click', () => {
-
-y =randomComputer(3)
+  main.addEventListener("click", () => {
+    y = randomComputer(3);
     step1.classList.add("step-1-minimize");
     step2.classList.add("step-2-minimizer");
-    
 
-    const div1 = document.createElement('DIV');
-    div1.setAttribute('class','option')
-    classListDesMains = main.classList[1]
-    let img  =document.createElement('IMG')
-    img.setAttribute("alt",'option')
-    img.setAttribute("src", "icon-"+classListDesMains+".svg")
-    replaces[0].append(div1)
-    replaces[0].classList.remove('replace')
-    replaces[0].classList.add(classListDesMains)
-    div1.append(img)
+    const div1 = document.createElement("DIV");
+    div1.setAttribute("class", "option");
+    classListDesMains = main.classList[1];
+    let img = document.createElement("IMG");
+    img.setAttribute("alt", "option");
+    img.setAttribute("src", "icon-" + classListDesMains + ".svg");
+    replaces[0].append(div1);
+    replaces[0].classList.remove("replace");
+    replaces[0].classList.add(classListDesMains);
+    div1.append(img);
+
+    let replaceElement = tab[y];
+    const div2 = document.createElement("DIV");
+    div2.setAttribute("class", "option");
+    classListDesMainsComputer = tab[y].classList[1];
+    let img2 = document.createElement("IMG");
+    img2.setAttribute("alt", "option");
+    img2.setAttribute("src", "icon-" + classListDesMainsComputer + ".svg");
+    replaces[1].append(div2);
+    replaces[1].classList.remove("replace");
+    replaces[1].classList.add(classListDesMainsComputer);
+    div2.append(img2);
+
+    playAgainButton.addEventListener("click", () => {
+      step1.classList.remove("step-1-minimize");
+      step2.classList.remove("step-2-minimizer");
+      replaces[0].classList.remove(classListDesMains);
+      replaces[0].removeChild(div1);
+      replaces[1].classList.remove(classListDesMainsComputer);
+      replaces[1].removeChild(div2);
+    });
+
+    let fonctionText = comptageDesPoints(
+      classListDesMains,
+      classListDesMainsComputer
+    );
     
-           
-    let replaceElement = tab[y]
-    const div2 = document.createElement('DIV');
-    div2.setAttribute('class','option')
-    classListDesMainsComputer = tab[y].classList[1]
-    let img2  =document.createElement('IMG')
-    img2.setAttribute("alt",'option')
-    img2.setAttribute("src", "icon-"+classListDesMainsComputer+".svg")
-    replaces[1].append(div2)
-    replaces[1].classList.remove('replace')
-    replaces[1].classList.add(classListDesMainsComputer)
-    div2.append(img2)
+    WinnerLooserText.textContent = fonctionText;
+
+    if (fonctionText == "You win") {
+      resultat = resultat + 1;
+    } else if (fonctionText == "You Lose") {
+      resultat = resultat - 1;
+    }
+    
+    localStorage.setItem("resultatValue", resultat);
+    scoreValue.textContent = localStorage.getItem("resultatValue");
+  });
 
   
-    if(classListDesMains == "rock" && classListDesMainsComputer == "scissors"){
-        WinnerLooserText.innerHTML='You win'
-        resultat++
-    }else if (classListDesMains == "scissors" && classListDesMainsComputer == "paper"){
-        WinnerLooserText.innerHTML='You win'
-        resultat++
-    }else if(classListDesMains == "paper" && classListDesMainsComputer == "rock"){
-        WinnerLooserText.innerHTML='You win'
-        resultat++
-    }else if  (classListDesMains == classListDesMainsComputer ){
-        WinnerLooserText.innerHTML='Equality'
-        
-    }else{
-        WinnerLooserText.innerHTML='You Loose'
-        resultat--
-    }
-})
-        scoreValue.textContent=resultat
+});
 
-playAgainButton.addEventListener("click", () => {
-        step1.classList.remove("step-1-minimize");
-        step2.classList.remove("step-2-minimizer");
-        
-      });
-
-    })
-
-
-
+function comptageDesPoints(a, b) {
+  if (a == "rock" && b == "scissors") {
+    return "You win";
+  } else if (a == "scissors" && b == "paper") {
+    return "You win";
+  } else if (a == "paper" && b == "rock") {
+    return "You win";
+  } else if (a == b) {
+    return "Equality";
+  } else {
+    return "You Lose";
+  }
+}

3aa760336ec815d3df7e7a167275e0ed67657cb1
AlexOnWax <alexandre.barbet@hotmail.com>
2022-06-09 11:08:12 +0200

    Projet Finis

--- projet_html_code/game.js
+++ projet_html_code/game.js
@@ -15,17 +15,23 @@ const compPickReplace = document.querySelector(".computer-pick > .replace");
 const tabPfp = [scissors, rock, paper];
 const tab = [scissors2, rock2, paper2];
 const scoreValue = document.querySelector(".score-value");
+
+scoreValue.textContent = localStorage.getItem("resultatValue");
+
 const imgScissors = document.querySelector(".scissors > option > img ");
 const WinnerLooserText = document.querySelector(".annouce-winner-text");
 let resultat = 0;
 
-
 function randomComputer(x) {
   return Math.floor(Math.random() * x);
 }
+  
 
 lesMains.forEach((main) => {
+  
+  
   main.addEventListener("click", () => {
+    
     y = randomComputer(3);
     step1.classList.add("step-1-minimize");
     step2.classList.add("step-2-minimizer");
@@ -33,6 +39,7 @@ lesMains.forEach((main) => {
     const div1 = document.createElement("DIV");
     div1.setAttribute("class", "option");
     classListDesMains = main.classList[1];
+   
     let img = document.createElement("IMG");
     img.setAttribute("alt", "option");
     img.setAttribute("src", "icon-" + classListDesMains + ".svg");
@@ -44,7 +51,7 @@ lesMains.forEach((main) => {
     let replaceElement = tab[y];
     const div2 = document.createElement("DIV");
     div2.setAttribute("class", "option");
-    classListDesMainsComputer = tab[y].classList[1];
+    classListDesMainsComputer = replaceElement.classList[1];
     let img2 = document.createElement("IMG");
     img2.setAttribute("alt", "option");
     img2.setAttribute("src", "icon-" + classListDesMainsComputer + ".svg");
@@ -52,10 +59,13 @@ lesMains.forEach((main) => {
     replaces[1].classList.remove("replace");
     replaces[1].classList.add(classListDesMainsComputer);
     div2.append(img2);
+    replaces[0].style.pointerEvents = "none";
+    replaces[1].style.pointerEvents = "none";
 
     playAgainButton.addEventListener("click", () => {
       step1.classList.remove("step-1-minimize");
       step2.classList.remove("step-2-minimizer");
+
       replaces[0].classList.remove(classListDesMains);
       replaces[0].removeChild(div1);
       replaces[1].classList.remove(classListDesMainsComputer);
@@ -66,7 +76,7 @@ lesMains.forEach((main) => {
       classListDesMains,
       classListDesMainsComputer
     );
-    
+
     WinnerLooserText.textContent = fonctionText;
 
     if (fonctionText == "You win") {
@@ -74,13 +84,20 @@ lesMains.forEach((main) => {
     } else if (fonctionText == "You Lose") {
       resultat = resultat - 1;
     }
-    
+
     localStorage.setItem("resultatValue", resultat);
     scoreValue.textContent = localStorage.getItem("resultatValue");
   });
 
-  
+
 });
+
+
+
+
+
+
+
 
 function comptageDesPoints(a, b) {
   if (a == "rock" && b == "scissors") {
